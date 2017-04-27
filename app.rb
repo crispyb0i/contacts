c20e2798f70e4007793f67e76785b60fde24ea58
David Shin and Max Reichman <david@>
2017-04-27 11:42:06 -0700

    Make it better.

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/contact')
+require('./lib/email_address')
+require('./lib/mailing_address')
+require('./lib/phone_number')
 also_reload('lib/**/*.rb')
 
 get('/') do
@@ -8,6 +11,39 @@ get('/') do
 end
 
 get('/contacts') do
-  # @title = (params.fetch('title1')).rps()
+  @contacts = Contact.all()
   erb(:contacts)
+end
+
+get('/contacts/new') do
+  erb(:contacts_form)
+end
+
+post('/contacts') do
+  first_name = params.fetch('first_name')
+  last_name = params.fetch('last_name')
+  job_title = params.fetch('job_title')
+  company = params.fetch('company')
+  Contact.new({:first_name => first_name, :last_name => last_name, :job_title => job_title, :company => company}).save()
+  @contacts = Contact.all()
+  erb(:contacts)
+end
+
+get('/contacts/:id') do
+  @contact = Contact.find(params.fetch('id').to_i())
+  erb(:contact)
+end
+
+get('/contacts/:id/emails/new') do
+  @contact = Contact.find(params.fetch('id').to_i())
+  erb(:email_address_form)
+end
+
+post('/contacts/:id') do
+  email = params.fetch('email')
+  @email = Email_address.new({:email => email})
+  contact = Contact.find(params.fetch('id').to_i())
+  contact.add_email_address(@email)
+  @contact = contact
+  erb(:contact)
 end
--- lib/contact.rb
+++ lib/contact.rb
@@ -1,5 +1,8 @@
 class Contact
-  attr_accessor(:first_name, :last_name, :job_title, :company, :mailing_addresses, :phone_numbers, :emails)
+
+  @@contacts = []
+
+  attr_accessor(:first_name, :last_name, :job_title, :company, :mailing_addresses, :phone_numbers, :emails, :id)
 
   define_method(:initialize) do |attributes|
     @first_name = attributes.fetch(:first_name)
@@ -9,6 +12,7 @@ class Contact
     @mailing_addresses = []
     @phone_numbers = []
     @emails = []
+    @id = @@contacts.length().+(1)
   end
 
   define_method(:add_mailing_address) do |mailing_address|
@@ -22,4 +26,26 @@ class Contact
   define_method(:add_email_address) do |email_address|
     @emails.push(email_address)
   end
+
+  define_singleton_method(:all) do
+   @@contacts
+ end
+
+ define_method(:save) do
+   @@contacts.push(self)
+ end
+
+ define_singleton_method(:clear) do
+   @@contacts = []
+ end
+
+define_singleton_method(:find) do |identification|
+  found_contact = nil
+  @@contacts.each() do |contact|
+    if contact.id.eql?(identification)
+      found_contact = contact
+    end
+  end
+  found_contact
+ end
 end
